4aa638edc03e0ce498991994ea6d92e57bda4e2d
Ericko Samudera <ericko.samudera@vidio.com>
2025-10-03 14:34:11 +0700

    implement app list menu

--- app/src/main/java/me/samud/minimumlauncher/ListItem.kt
+++ app/src/main/java/me/samud/minimumlauncher/ListItem.kt
@@ -1,22 +1,54 @@
 package me.samud.minimumlauncher
 
 import android.content.Context
-import androidx.fragment.app.FragmentManager
-
+import android.content.Intent
+import android.net.Uri
+import android.provider.Settings
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import kotlin.reflect.KClass
+
 
 sealed class ListItem {
     interface Launchable {
         fun onLaunch(context: Context, fragmentManager: FragmentManager?)
     }
 
-    data class UserAppItem(val appInfo: AppInfo) : ListItem(), Launchable {
+    interface LongClickable {
+        fun onLongClick(context: Context, fragmentManager: FragmentManager?): Boolean
+    }
+
+    data class UserAppItem(val appInfo: AppInfo) : ListItem(), Launchable, LongClickable {
         override fun onLaunch(context: Context, fragmentManager: FragmentManager?) {
             val launchIntent = context.packageManager.getLaunchIntentForPackage(appInfo.packageName)
             launchIntent?.let {
                 context.startActivity(it)
             }
+        }
+
+        override fun onLongClick(context: Context, fragmentManager: FragmentManager?): Boolean {
+            val bottomSheetDialog = BottomSheetDialog(context)
+            val view = LayoutInflater.from(context).inflate(R.layout.bottom_sheet_menu, null)
+            val menuLayout = view.findViewById<LinearLayout>(R.id.menu_layout)
+
+            // "App info" item
+            val appInfoItem = LayoutInflater.from(context).inflate(R.layout.bottom_sheet_menu_item, menuLayout, false) as TextView
+            appInfoItem.text = "App info"
+            appInfoItem.setOnClickListener {
+                val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                intent.data = Uri.fromParts("package", appInfo.packageName, null)
+                context.startActivity(intent)
+                bottomSheetDialog.dismiss()
+            }
+            menuLayout.addView(appInfoItem)
+
+            bottomSheetDialog.setContentView(view)
+            bottomSheetDialog.show()
+            return true
         }
     }
 
@@ -30,9 +62,14 @@ sealed class ListItem {
         }
     }
 
-    data class WebShortcutItem(val title: String, val url: String) : ListItem(), Launchable {
+    data class WebShortcutItem(val title: String, val url: String) : ListItem(), Launchable, LongClickable {
         override fun onLaunch(context: Context, fragmentManager: FragmentManager?) {
             // TODO: Not yet implemented
+        }
+
+        override fun onLongClick(context: Context, fragmentManager: FragmentManager?): Boolean {
+            // TODO: Not yet implemented
+            return true
         }
     }
 
