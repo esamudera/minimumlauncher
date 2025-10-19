9231d1d8fa19c433310731f8922d4ff8efec068f
Ericko Samudera <ericko.samudera@vidio.com>
2025-10-19 21:49:45 +0700

    Revert "feat: add Manage Widgets activity to app list"
    This reverts commit d7f977d6f8206626da8b105ef04ab83dc033dcbe.

--- app/src/main/java/me/samud/minimumlauncher/AppListViewModel.kt
+++ app/src/main/java/me/samud/minimumlauncher/AppListViewModel.kt
@@ -87,12 +87,6 @@ class AppListViewModel(application: Application) : AndroidViewModel(application)
                 intent = android.content.Intent(context, SettingsActivity::class.java)
             )
         )
-        displayList.add(
-            ListItem.InternalActivityItem(
-                title = context.getString(R.string.title_manage_widgets),
-                intent = android.content.Intent(context, ManageWidgetsActivity::class.java)
-            )
-        )
 
         return displayList
     }
